1658edc5fcfa5c314cbff0d49adde5e420fe5170
Johannes Segerlund <js224em@student.lnu.se>
2020-04-27 12:53:46 +0200

    server1

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 const router = require('./routes/router')
 const expha = require('express-handlebars')
+const session = require('express-session')
+
+require('dotenv').config()
 
 app.use(express.static('public'))
 const hbs = expha.create({
@@ -18,9 +21,22 @@ app.engine('handlebars', hbs.engine)
 
 app.set('view engine', 'handlebars')
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false
+  }
+}))
+
 app.use('/', router)
 
-http.listen(3000, function () {
+const port = process.env.PORT || '3000'
+
+app.set('port', port)
+
+http.listen(port, function () {
   console.log('listening on *:3000')
 })
 
@@ -28,4 +44,13 @@ io.on('connection', (socket) => {
   console.log('connected')
   socket.emit('message', 'connected sucsessfully')
   socket.emit('index')
+})
+
+app.use((req, res, next) => {
+  res.status(404).send('not found 404')
+})
+
+app.use((err, req, res, next) => {
+  console.log(err.stack)
+  res.status(500).send('Ups something dident go correctly 500')
 })
